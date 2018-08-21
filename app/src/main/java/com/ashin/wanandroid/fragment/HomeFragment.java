c706fe2560ad997dd0577e66fafae5d37e31005e
xuwenliang <ashin0928@163.com>
2018-08-21 17:11:49 +0800

    首页界面

--- app/src/main/java/com/ashin/wanandroid/fragment/HomeFragment.java
+++ app/src/main/java/com/ashin/wanandroid/fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.ashin.wanandroid.fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -19,31 +20,38 @@ import com.ashin.wanandroid.bean.BannerBean;
 import com.ashin.wanandroid.bean.GetArticleListResult;
 import com.ashin.wanandroid.network.BaseResult;
 import com.ashin.wanandroid.network.RetrofitHelper;
+import com.ashin.wanandroid.utils.GlideImageLoader;
 import com.ashin.wanandroid.utils.RxUtils;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+import com.youth.banner.Banner;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class HomeFragment extends BaseFragment {
 
+
+
     @BindView(R.id.smfl)
     SmartRefreshLayout refreshLayout;
-
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
 
 
     public static final String TAG = HomeFragment.class.getSimpleName();
-    private List<BannerBean> data;
     private List<ArticleBean> articles;
+    private List<BannerBean> bannerData;
+    private View bannerLayout;
+    private int index = 0;
+    private HomeAdapter homeAdapter;
+    private Banner banner;
+    private View bannerView;
 
     public static HomeFragment getInstance() {
         HomeFragment fragment = new HomeFragment();
@@ -68,61 +76,86 @@ public class HomeFragment extends BaseFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         initView();
         initData();
+
     }
 
     private void initData() {
+        getBanner();
 
-        RetrofitHelper.init().getArticleList(0)
+        getArticleList();
+    }
+
+    @SuppressLint("CheckResult")
+    private void getArticleList() {
+
+        RetrofitHelper.init().getArticleList(index)
                 .compose(RxUtils.<BaseResult<GetArticleListResult>>switchSchelers())
-                .subscribe(new Observer<BaseResult<GetArticleListResult>>() {
+                .subscribe(new Consumer<BaseResult<GetArticleListResult>>() {
                     @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(BaseResult<GetArticleListResult> result) {
-                        GetArticleListResult data = result.getData();
-                        articles = data.getDatas();
-
-                        Log.v(Const.LOG_TAG,"result = "+result.isSuccess());
-                        updateView();
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
+                    public void accept(BaseResult<GetArticleListResult> getArticleListResultBaseResult) throws Exception {
+                        GetArticleListResult data = getArticleListResultBaseResult.getData();
+                        index = data.getCurPage();
+                        if (index == 1){
+                            articles = data.getDatas();
+                            homeAdapter.replaceData(articles);
+                        }else {
+                            articles.addAll(data.getDatas());
+                            homeAdapter.addData(data.getDatas());
+                        }
                     }
                 });
 
     }
 
+
+
+    @SuppressLint("CheckResult")
+    private void getBanner() {
+        RetrofitHelper.init().getBanner()
+                .compose(RxUtils.<BaseResult<List<BannerBean>>>switchSchelers())
+               .subscribe(new Consumer<BaseResult<List<BannerBean>>>() {
+                   @Override
+                   public void accept(BaseResult<List<BannerBean>> listBaseResult) throws Exception {
+                       bannerData = listBaseResult.getData();
+                       updateBanner();
+                   }
+               });
+    }
+
     private void updateView() {
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        HomeAdapter homeAdapter = new HomeAdapter(R.layout.item_article_layout, articles);
-        recyclerView.setAdapter(homeAdapter);
+
 
     }
 
+
     private void updateBanner() {
 
+        banner.setImageLoader(new GlideImageLoader());
+        banner.setImages(bannerData);
+        banner.start();
 
     }
 
     private void initView() {
+        bannerView = LayoutInflater.from(getActivity()).inflate(R.layout.banner_layout, null);
+        banner = bannerView.findViewById(R.id.banner_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        homeAdapter = new HomeAdapter(R.layout.item_article_layout, articles);
+        recyclerView.setAdapter(homeAdapter);
+        homeAdapter.addHeaderView(bannerView);
 
         refreshLayout.setOnRefreshListener(new OnRefreshListener() {
             @Override
             public void onRefresh(@NonNull RefreshLayout refreshLayout) {
                 //刷新
-                Log.v(Const.LOG_TAG,"onRefresh");
+                Log.v(Const.LOG_TAG, "onRefresh");
+                index = 0;
+                initData();
+                refreshLayout.finishRefresh(2000);
             }
         });
 
@@ -130,7 +163,10 @@ public class HomeFragment extends BaseFragment {
             @Override
             public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
                 //加载更多
-                Log.v(Const.LOG_TAG,"onLoadMore");
+                Log.v(Const.LOG_TAG, "onLoadMore");
+                index++;
+                getArticleList();
+                refreshLayout.finishLoadMore(2000);
             }
         });
 
---
+++ app/src/main/java/com/ashin/wanandroid/utils/GlideImageLoader.java
@@ -0,0 +1,22 @@
+package com.ashin.wanandroid.utils;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.ashin.wanandroid.bean.BannerBean;
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+
+/**
+ * @author quchao
+ * @date 2018/3/5
+ */
+
+public class GlideImageLoader extends ImageLoader {
+
+    @Override
+    public void displayImage(Context context, Object o, ImageView imageView) {
+        String path = ((BannerBean) o).getImagePath();
+        Glide.with(context).load(path).into(imageView);
+    }
+}
